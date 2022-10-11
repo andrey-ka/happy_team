9f7c94a5368d8f8161f198a988dab62ce1485871
andrey-ka <andrey.kaban@mail.com>
2022-10-11 16:47:29 +0300

    (NT) home page and seeds init

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+class Notifier
+  def initialize
+    @messages = []
+  end
+  def add_summary(string)
+    messages << ' '
+    messages << "===> #{string}..."
+  end
+
+  def add_message(string)
+     messages << "  #{string}"
+  end
+
+  def print
+    puts; puts 'rake db:seed summary:'
+    messages.each { |m| puts m }
+    puts
+  end
+
+  private
+
+  attr_reader :messages
+end
+
+@notifier = Notifier.new
+
+@notifier.add_summary "creating users"
+
+@notifier.add_message "user 1"
+
+@notifier.print
